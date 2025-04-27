1cb2074b11c57329fa3111b1761c9f3b0e48bd21
KirisameVanilla <vanillaaaajp@gmail.com>
2025-04-27 19:28:59 +0800

    feat: consistancy storage of settings

---
+++ MajdataPlayUpdater/Models/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajdataPlayUpdater.Models;
+public class Settings
+{
+    public string LastOpenedFolder { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+    public string ProxyUrl { get; set; } = "";
+}
---
+++ MajdataPlayUpdater/ViewModels/SettingsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using MajdataPlayUpdater.Models;
+
+namespace MajdataPlayUpdater.ViewModels;
+
+public static class SettingsManager
+{
+    private static readonly string SettingsFilePath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "MajdataPlayUpdater",
+        "settings.json");
+
+    public static Settings Settings { get; private set; } = new Settings();
+
+    public static void Load()
+    {
+        try
+        {
+            if (File.Exists(SettingsFilePath))
+            {
+                var json = File.ReadAllText(SettingsFilePath);
+                Settings = JsonSerializer.Deserialize<Settings>(json, JsonContext.IndentedOptions) ?? new Settings();
+            }
+        }
+        catch
+        {
+            Settings = new Settings();
+        }
+    }
+
+    public static void Save()
+    {
+        try
+        {
+            var directory = Path.GetDirectoryName(SettingsFilePath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory!);
+            }
+
+            var json = JsonSerializer.Serialize(Settings, JsonContext.IndentedOptions);
+            File.WriteAllText(SettingsFilePath, json);
+        }
+        catch
+        {
+            
+        }
+    }
+}

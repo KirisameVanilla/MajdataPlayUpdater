ec08e6d71f115a7b791b9980d0c5fd35166991c5
KirisameVanilla <vanillaaaajp@gmail.com>
2025-05-05 08:13:08 +0800

    perf: move url to settings

--- MajdataPlayUpdater/Models/Settings.cs
+++ MajdataPlayUpdater/Models/Settings.cs
@@ -6,4 +6,6 @@ public class Settings
 {
     public string LastOpenedFolder { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
     public string ProxyUrl { get; set; } = "";
+    public string HashJsonEndPoint { get; set; } = "https://majdataplay-distrib.work/";
+    public string DownloadEndPoint { get; set; } = "https://majdataplay-distrib.work/";
 }
